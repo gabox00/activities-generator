8d3a46d1112c86ed5a8661401a769df297d6b0e2
gabox00 <gabogutz@gmail.com>
2022-11-10 00:54:34 +0100

    imprimimos errores del crud de las actividades

--- views/activity/index.php
+++ views/activity/index.php
@@ -1,5 +1,18 @@
 <div class="container">
+    <div>
+        <!--    Pintamos cualquier error del CRUD de la actividad    -->
+        <?php if(!empty($_SESSION['errors']['activity'])): ?>
+            <?php foreach ($_SESSION['errors']['activity'] as $error): ?>
+                <div class="mt-4">
+                    <div class="alert alert-danger mt-1" role="alert">
+                        <?= $error[0]; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
     <div class="h-100 d-flex justify-content-<?=!empty($_SESSION['user']['activities']) ? 'around' : 'center'?> align-items-center ">
+        <!--    Incluimos las vistas    -->
         <?php require_once("update.php") ?>
         <?php require_once("create.php") ?>
     </div>
